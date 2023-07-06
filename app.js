a02beb7c94627a44fa9f427dfcf9e20d109f4eac
Deucalino Marildo Muvuma <muvumafernando@gmail.com>
2023-07-06 20:00:44 +0000

    errors corrected, but error 502 is showed during display

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
-let article={'The','A',}
-let adjective={'Quiet','Happy','Crappy'}
-let name={'Man', 'Cat', 'Town', 'Dude'}
-let extension= {'.com', '.net', '.cat'}
-fuction domainNameGenerator () {
+
+let article=['The','A',]
+let adjective=['Quiet','Happy','Crappy']
+let name=['Man', 'Cat', 'Town', 'Dude']
+let extension= ['.com', '.net', '.cat']
+function domainNameGenerator () {
     let randomArticle = article[Math.floor(Math.random() * article.length)];
   let randomAdjective = adjective[Math.floor(Math.random() * adjective.length)];
   let randomName = name[Math.floor(Math.random() * name.length)];
   let randomExtension= extension[Math.floor(Math.random() * extension.length)];
   return randomArticle + randomAdjective + randomName + randomExtension;
 }
-var domainNameGenerator=fuction domainNameGenerator ()
-console.log(domainNameGenerator);
+window.onload=function(){
+  var s= document.getElementById("domainNameGenerator").innerHTML;
+  console.log(domainNameGenerator); 
+}
+var domainNameGenerator=fuction domainNameGenerator 
+ 
+  
+
+
