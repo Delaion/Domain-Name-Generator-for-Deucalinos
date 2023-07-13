b0d2a616960f1f5ac24ddd946610b998bd9ea2b7
Deucalino Marildo Muvuma <muvumafernando@gmail.com>
2023-07-13 13:56:54 +0000

    random function erased and substituted by for loop to generate Domain Names by iteration, even the compilation is done, the expected result is not shown in browser

--- app.js
+++ app.js
@@ -1,20 +1,27 @@
+const { name } = require("file-loader")
 
-let article=['The','A',]
+let article=['The','A','My']
 let adjective=['Quiet','Happy','Crappy']
 let name=['Man', 'Cat', 'Town', 'Dude']
 let extension= ['.com', '.net', '.cat']
-function domainNameGenerator () {
-    let randomArticle = article[Math.floor(Math.random() * article.length)];
-  let randomAdjective = adjective[Math.floor(Math.random() * adjective.length)];
-  let randomName = name[Math.floor(Math.random() * name.length)];
-  let randomExtension= extension[Math.floor(Math.random() * extension.length)];
-  return randomArticle + randomAdjective + randomName + randomExtension;
+
+for (let i = 0; i < article.length; i++) {
+  for (let j = 0; j < adjective.length; j++) {
+    for (let k = 0; k < name.length; k++) {
+      for(let l=0; l< extension.length; l++){
+      let domainNameGenerator = article[i] + adjective[j] + name[k] + extension[l];
+      console.log(domainNameGenerator);
+      }
+    }
+  }
 }
+        
+      
 window.onload=function(){
   var s= document.getElementById("domainNameGenerator").innerHTML;
   console.log(domainNameGenerator); 
 }
-var domainNameGenerator=fuction domainNameGenerator 
+
  
   
 
